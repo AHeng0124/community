4ad2312068adce81be47185a34f69bfba0bc1d34
AHeng0124 <529680821@qq.com>
2020-03-30 20:53:02 +0800

    extract github congig from code to pro per ties

---
+++ src/main/java/com/qh/community/controller/AuthorizeController.java
@@ -0,0 +1,40 @@
+package com.qh.community.controller;
+
+import com.qh.community.dto.AccessTokenDto;
+import com.qh.community.dto.GitHubUser;
+import com.qh.community.provider.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GitHubProvider gitHubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name = "state")String state){
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
+        accessTokenDto.setCode(code);
+        accessTokenDto.setRedirect_uri(redirectUri);
+        accessTokenDto.setState(state);
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDto);
+        GitHubUser user = gitHubProvider.gitUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
